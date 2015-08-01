7092f3330688704e30f3df628e1a06912267aaae
Paulo Fidalgo <fidalgo@gmx.com>
2015-08-01 11:52:04 +0100

    Complete the User model adding tests and a factory.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+context "Create a valid user" do
+  it "creates a valid user from the Factory" do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
+end
+
 end
