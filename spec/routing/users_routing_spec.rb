b7792abaac2c617135734e968afb45177c99abf9
Paulo Fidalgo <fidalgo@gmx.com>
2015-08-03 17:02:00 +0100

    Deleted forgotten UsersController and update the routing spec to match the correct controller class.

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -1,15 +1,11 @@
 require "rails_helper"
 
-RSpec.describe UsersController, type: :routing do
+RSpec.describe Api::V1::UsersController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
       expect(:get => "/api/v1/users").to route_to("api/v1/users#index")
     end
-
-    # it "routes to #new" do
-    #   expect(:get => "/api/v1/users/new").to route_to("api/v1/users#new")
-    # end
 
     it "routes to #show" do
       expect(:get => "/api/v1/users/1").to route_to("api/v1/users#show", :id => "1")
