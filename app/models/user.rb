09db7648a9da0bca62e1be93569cc25796198b1c
Paulo Fidalgo <fidalgo@gmx.com>
2015-08-10 22:15:32 +0100

    Use Enums with values and add an index to user role.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   after_initialize :default_role
 
-  enum role: [ :admin, :user, :guest ]
+  enum role: {admin: 0, user: 1, guest: 2 }
 
   validates_presence_of :email
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
--- db/migrate/20150803092109_add_role_to_user.rb
+++ db/migrate/20150803092109_add_role_to_user.rb
@@ -1,5 +1,5 @@
 class AddRoleToUser < ActiveRecord::Migration
   def change
-    add_column :users, :role, :integer, default: 0
+    add_column :users, :role, :integer, default: 0, index: true
   end
 end
